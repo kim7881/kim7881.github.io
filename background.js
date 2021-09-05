3a87bb960bd6ca7aaec6d61352239d701b5867b3
kim7881 <86556384+kim7881@users.noreply.github.com>
2021-09-05 23:41:09 +0900

    Update background.js

--- background.js
+++ background.js
@@ -9,7 +9,7 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 const body = document.querySelector("body");
 
 function back(){
-    body.style.backgroundImage = `url(${chosenImage})`;
+    body.style.backgroundImage = `url(img/${chosenImage})`;
 }
 
 back();
