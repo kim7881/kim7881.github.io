f9f4f5f108eb60b3f1ea504a7aba714560e76273
kim7881 <86556384+kim7881@users.noreply.github.com>
2021-09-05 23:11:23 +0900

    Create weather.js

---
+++ weather.js
@@ -0,0 +1,23 @@
+const API_KEY ="49921f5abde9c16e3ff5a497300ee795";
+
+function onGeoOk(position){
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    // console.log("You live in", lat, lng);
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then((response) => response.json()
+    .then((data) => {
+        const city = document.querySelector("#weather span:first-child");
+        const weather = document.querySelector("#weather span:last-child");
+        console.log(city)
+        city.innerText = `${data.name} :`;
+        weather.innerText = `${data.weather[0].main}/ ${data.main.temp}°c`;
+    }));
+}
+
+function onGeoError(){
+    alert("Can't find you, No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
